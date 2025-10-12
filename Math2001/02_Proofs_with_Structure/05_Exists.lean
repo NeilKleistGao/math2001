8b792059f3e0a1e2e0a182c058045f4d26e54ca7
NeilKleistGao <neilkleist@gmail.com>
2025-10-12 12:53:42 +0800

    Update

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -21,7 +21,18 @@ example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
     cancel -x at hxt'
     apply ne_of_gt
     apply hxt'
-  · sorry
+  · have hxt' : 0 < -x * t := by addarith [hxt]
+    have ht :=
+    calc
+      x * (-t) = (-x) * t := by ring
+      _ > 0 := by rel [hxt']
+    have ht' := by cancel x at ht
+    apply ne_of_lt
+    calc
+      t = -1 * (-t) := by ring
+      _ < -1 * 0 := by addarith [ht']
+      _ = 0 := by numbers
+
 
 example : ∃ n : ℤ, 12 * n = 84 := by
   use 7
@@ -34,13 +45,22 @@ example (x : ℝ) : ∃ y : ℝ, y > x := by
 
 
 example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 := by
-  sorry
+  use 6, 5
+  numbers
 
 example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 := by
-  sorry
+  use a + 1, a
+  ring
 
 example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q := by
-  sorry
+  use (p + q) / 2
+  constructor
+  · calc
+    p = (p + p) / 2 := by ring
+    _ < (p + q) / 2 := by rel [h]
+  · calc
+    q = (q + q) / 2 := by ring
+    _ > (p + q) / 2 := by rel [h]
 
 example : ∃ a b c d : ℕ,
     a ^ 3 + b ^ 3 = 1729 ∧ c ^ 3 + d ^ 3 = 1729 ∧ a ≠ c ∧ a ≠ d := by
